1c576cff6faf8b86e4d0e7f86bc18054c49e22a7
Hazem Elmasry <hazemelmasryofficial@gmail.com>
2026-02-02 03:31:18 +0200

    Enhance AppController and AppService with additional endpoints and methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,55 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("/home")
+  getHome(): string {
+    return this.appService.getHome();
+  }
+
+  @Post("/user")
+  getUser(@Query() qu): any {
+    return qu;
+    return this.appService.getUser();
+  }
+
+  // Query params
+  @Post("/user2")
+  getUser2(@Query() username2): any {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    return username2.name;
+  }
+
+  // Path params
+  @Post("/user3/:name")
+  getUser3(@Param() param): any {
+    return param;
+  }
+
+  // Body params
+  @Post("/user4")
+  getUser4(@Body() body): any {
+    return body;
+  }
+
+  // return specific param from the body in response
+  @Post("/user5")
+  getUser5(@Body("name") name: string) {
+    return this.appService.sayHelloTo(name);
+  }
+
+  // Query params with specific name(param)
+  @Post("/country")
+  getCountry(@Query("name") countryName): any {
+    return countryName;
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,16 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getHome(): string{
+    return "Home";
+  }
+
+  getUser(): string{
+    return "Hazem";
+  }
+
+  sayHelloTo(name:string) {
+    return "Hello to: " + name;
+  }
 }
