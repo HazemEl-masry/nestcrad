0d66f47392dbd7e9a0197ed631fe595489c6497c
Hazem Elmasry <hazemelmasryofficial@gmail.com>
2026-02-03 02:14:14 +0200

    feat(database): add TypeORM integration with MySQL and define Payment entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,23 @@ import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { PaymentModule } from './payment/payment.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from './payment/entities/payment.entity';
 
 @Module({
-  imports: [PaymentModule],
+  imports: [
+    PaymentModule,
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "nestuser",
+      password: "123456",
+      database: "nestcrad",
+      entities: [Payment],
+      synchronize: true,
+    })
+  ],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
--- src/payment/entities/payment.entity.ts
+++ src/payment/entities/payment.entity.ts
@@ -1,1 +1,29 @@
-export class Payment {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+
+@Entity()
+export class Payment {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 500 })
+    name: string;
+
+    @Column('text')
+    description: string;
+
+    @Column()
+    price: string;
+
+    @Column("text")
+    comment: string;
+
+    @Column('int')
+    views: number;
+
+    @Column()
+    isPublished: boolean;
+
+    @Column("int")
+    likes: number;
+}
